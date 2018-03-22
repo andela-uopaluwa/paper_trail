dc78890c94805a1a48bdec9d51a50913e8bed4ef
andela-uopaluwa <ufedojo.opaluwa@andela.com>
2018-03-22 23:06:01 +0100

    setup gulp task for sass compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 const gulp = require('gulp');
 const connect = require('gulp-connect');
+const sass = require('gulp-sass');
 
 //TODO: fill in gulp tasks
-
-gulp.task('default', function() {
-    console.log('default task running');
+gulp.task('sass', function() {
+  return gulp.src('public/sass/style.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('public/dist'))
 });
+
+gulp.task('sass:watch', function() {
+  gulp.watch('public/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass']);
